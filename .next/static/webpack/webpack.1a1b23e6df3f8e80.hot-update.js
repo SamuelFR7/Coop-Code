b3a194fa8ffbf008f66fedbc3dfefc7d7645db38
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-05-24 15:55:13 -0300

    feat: next gitignore

--- .next/static/webpack/webpack.1a1b23e6df3f8e80.hot-update.js
+++ .next/static/webpack/webpack.1a1b23e6df3f8e80.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "67ea482b4104d177"; }
+/******/ 	__webpack_require__.h = function() { return "414ccbfd0915dbf7"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
